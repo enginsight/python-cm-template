3769cdc7839cec7d179b46591b0ef891f3eac226
Eric Range <ericrange@users.noreply.github.com>
2019-01-25 20:03:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,8 @@ one_metric = random.randint(1,101)
 another_metric = random.randint(1,101)
 
 print("""
-
 __METRICS__={
   "one_metric": %d,
   "another_metric": %d
 }
-
 """ % (one_metric, another_metric))
